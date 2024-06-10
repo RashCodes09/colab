e4629c29685c44ef5798b379f8b75445d1aebe46
RashCodes09 <ayinkerasheedat500@gmail.com>
2024-06-10 04:41:09 -0700

    second

--- app/group1/page.tsx
+++ app/group1/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="w-full h-[70%] bg-red-900 flex justify-center items-center">
+      <div className="w[80%] h-[50%] text-white text-[19px]">anything</div>
+    </div>
+  );
 };
 
 export default page;
